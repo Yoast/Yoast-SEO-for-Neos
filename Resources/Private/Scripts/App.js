66e0b476f28c7de9a614edfaaaf429fc05ff047f
Sebastian Helzle <sebastian@helzle.it>
2017-12-21 11:44:44 +0700

    TASK: Make snippet fields inline editable

--- Resources/Private/Scripts/App.js
+++ Resources/Private/Scripts/App.js
@@ -16,13 +16,22 @@ import '../Styles/Main.scss';
     }
 
     window.onload = () => {
+        // Editable fields
         const focusKeywordField = document.getElementById('focusKeyword').querySelector('span');
+        const pageUriPathSegmentField = document.getElementById('pageUriPathSegment');
+        const seoTitleField = document.getElementById('seoTitle');
+        const metaDescriptionField = document.getElementById('metaDescription');
+        const editableFieldsWrap = document.getElementById('editable-fields-wrap');
+
+        // Containers for rendering
         const errorOutput = document.getElementById('errorOutput');
         const snippet = document.getElementById('snippet');
+
+        // Constants
         const previewUrl = document.getElementById('previewUrl').textContent;
-        const pageUriPathSegment = document.getElementById('pageUriPathSegment').textContent || '/';
         const baseUrl = document.getElementById('baseUrl').textContent;
 
+        // Generate preview then initialize plugin
         get(previewUrl)
             .then((previewDocument) => {
                 const parser = new DOMParser();
@@ -30,31 +39,34 @@ import '../Styles/Main.scss';
                 const metaSection = parsedPreviewDocument.querySelector('head');
                 const contentElements = parsedPreviewDocument.querySelectorAll('.neos-contentcollection');
 
+                // Store metadata in object to make easily accessible
                 const metaData = {
                     title: metaSection.querySelector('title').textContent,
                     description: metaSection.querySelector('meta[name="description"]').getAttribute('content')
                 };
 
+                // Concat all found content elements
                 let pageContent = '';
                 contentElements.forEach((element) => {
                     pageContent += element.innerHTML;
                 });
 
-                console.log(pageContent);
-
+                // Create snippet preview
                 const snippetPreviewContainer = new SnippetPreview({
                     data: {
                         title: metaData.title,
                         metaDesc: metaData.description,
-                        urlPath: pageUriPathSegment
+                        urlPath: pageUriPathSegmentField.textContent
                     },
                     placeholder: {
-                        urlPath: 'Enter the uri path segment here'
+                        urlPath: ''
                     },
+                    addTrailingSlash: false,
                     baseURL: baseUrl,
                     targetElement: snippet
                 });
 
+                // Initialize plugin
                 let app = new App({
                     snippetPreview: snippetPreviewContainer,
                     targets: {
@@ -76,9 +88,32 @@ import '../Styles/Main.scss';
                 const observer = new MutationObserver(function(mutations) {
                     app.refresh();
                 });
+                observer.observe(focusKeywordField, {characterData: true, subtree: true});
 
-                const config = {characterData: true, subtree: true};
-                observer.observe(focusKeywordField, config);
+                // Replace snippet editor form parts with Neos inline editing fields
+                const snippetEditorForm = snippet.querySelector('.snippet-editor__form');
+                snippetEditorForm.prepend(editableFieldsWrap);
+
+                // Move progress bars after new fields
+                seoTitleField.after(snippetEditorForm.querySelector('.snippet-editor__progress-title'));
+                metaDescriptionField.after(snippetEditorForm.querySelector('.snippet-editor__progress-meta-description'));
+
+                // Remove all original fields
+                snippetEditorForm.querySelectorAll('.snippet-editor__label').forEach((label) => {
+                    label.remove();
+                });
+
+                // Update snippet when editable fields are changed
+                const seoTitleObserver = new MutationObserver(function(mutations) {
+                    snippetPreviewContainer.setTitle(seoTitleField.textContent);
+                    snippetPreviewContainer.setUrlPath(pageUriPathSegmentField.textContent);
+                    snippetPreviewContainer.setMetaDescription(metaDescriptionField.textContent);
+                });
+
+                // Observe editable fields for changes
+                seoTitleObserver.observe(seoTitleField, {characterData: true, subtree: true});
+                seoTitleObserver.observe(pageUriPathSegmentField, {characterData: true, subtree: true});
+                seoTitleObserver.observe(metaDescriptionField, {characterData: true, subtree: true});
             })
             .catch((err) => {
                 errorOutput.textContent = err;
