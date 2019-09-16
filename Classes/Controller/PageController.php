fd1e83a8a9876de667a392f7868943ff18f4e68c
Sebastian Helzle <sebastian@helzle.it>
2019-09-16 14:13:30 +0200

    TASK: Use strict types in PHP

--- Classes/Controller/PageController.php
+++ Classes/Controller/PageController.php
@@ -1,19 +1,22 @@
 <?php
+declare(strict_types=1);
+
 namespace Yoast\YoastSeoForNeos\Controller;
 
 use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\Flow\Mvc\Controller\ActionController;
+use Neos\Flow\Mvc\Exception\StopActionException;
 
 class PageController extends ActionController
 {
 
-    /**
+    /**                                                                w
      * Redirects request to the given node in preview mode without the neos backend
      *
      * @param NodeInterface $node
-     * @throws \Neos\Flow\Mvc\Exception\StopActionException
+     * @throws StopActionException
      */
-    public function renderPreviewPageAction(NodeInterface $node)
+    public function renderPreviewPageAction(NodeInterface $node): void
     {
         $this->response->getHeaders()->setCacheControlDirective('no-cache');
         $this->response->getHeaders()->setCacheControlDirective('no-store');
